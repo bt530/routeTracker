3d551dd6a93b5d8a128ae004d99ca849e37b45a7
Hannes Brandstaetter-Mueller <Hannes.Brandstaetter@cloudflight.io>
2021-05-21 12:27:57 +0200

    changed log reader to utilize a JSON parser
    avoid manual parsing
    
    added requirements.txt for easier tracking of external dependencies

--- LogReader.py
+++ LogReader.py
@@ -1,7 +1,8 @@
+import json
 import os
 import fnmatch
-import datetime
 import logging
+import dateutil.parser
 
 from RouteData import RouteData
 
@@ -27,9 +28,8 @@ class LogReader:
         directory.reverse()
         self.log.debug("Directory content: " + str(directory))
 
-        # TODO use JSON library instead of custom parsing
         match = 1
-        target_match = 11 * 7 * 5 * 3 * 2
+        target_match = 7 * 5 * 3 * 2
         active_file_reached = False
         for i in range(len(directory)):
             self.log.info("Processing file %d: %s", i, directory[i])
@@ -47,128 +47,49 @@ class LogReader:
                     active_file_reached = True
                     self.log.debug('Active file reached')
                 try:
-                    with open(self.folder_location + "\\" + directory[i], 'rb') as f:
-                        scanning_file = f.read()
-                    scanning_file = scanning_file.decode('UTF-8')
+                    with open(self.folder_location + "\\" + directory[i], encoding="utf-8") as f:
+                        file_data = f.readlines()
 
                 except Exception:
-                    scanning_file = ''
+                    file_data = {}
                     self.log.exception('Error reading file ' + self.folder_location + "\\" + directory[i])
-                # Too much info self.log.debug('Full file content: ' + scanning_file)
 
-                if match % 2 != 0:
-                    scan = scanning_file
-                    scan = "generalJump".join(scan.split('"event":"CarrierJump"'))
-                    scan = "generalJump".join(scan.split('"event":"FSDJump"'))
-                    try:
-                        scan = scan.split('generalJump')
-                        if len(scan) != 1:
-                            scan = scan[-1].split('"StarSystem":"')[1].split('",')[0]
-                            self.log.info("Found current star system: " + scan)
-                            self.route_data.currentSystem = scan
+                for line in file_data:
+                    data = json.loads(line)
 
+                    if match % 2 != 0:
+                        if data["event"] == "CarrierJump" or data["event"] == "FSDJump":
+                            system = data["StarSystem"]
+                            self.log.info("Found current star system: " + system)
                             match = match * 2
-                    except Exception:
-                        self.log.exception("Problem reading current system")
-                        pass
 
-                if match % 3 != 0:
-
-                    scan = scanning_file
-
-                    try:
-                        scan = scan.split('Z", "event":"CarrierJumpRequest"')
-                        self.log.debug("CarrierJumpRequest: " + str(len(scan)))
-                        if len(scan) != 1:
-                            scan = scan[-2].split('"timestamp":"')[-1]
-                            self.log.debug("CarrierJumpRequest: " + scan)
-                            scan = scan.split('T')
-                            scan[0] = scan[0].split('-')
-                            scan[1] = scan[1].split(':')
-
-                            t = datetime.datetime(int(scan[0][0]), int(scan[0][1]), int(scan[0][2]), int(scan[1][0]),
-                                                  int(scan[1][1]), int(scan[1][2]))
-                            self.log.debug("CarrierJumpRequest: extracted " + str(t))
-
-                            t = t.replace(tzinfo=datetime.timezone.utc).timestamp()
+                    if match % 3 != 0:
+                        if data["event"] == "CarrierJumpRequest":
+                            t = data["timestamp"]
+                            self.log.debug("CarrierJumpRequest: extracted " + t)
+                            t = dateutil.parser.parse(t)
                             self.log.info("CarrierJumpRequest: found timestamp " + str(t))
-                            self.route_data.lastJumpRequest = t
-
+                            self.route_data.lastJumpRequest = t.timestamp()
                             match = match * 3
-                    except IndexError:
-                        self.log.exception("CarrierJumpRequest")
-                        pass
-                if match % 5 != 0:
 
-                    scan = scanning_file
+                    if match % 5 != 0:
+                        if data["event"] == "Cargo" and data["Vessel"] == "Ship":
+                            cargo = data["Count"]
+                            self.log.debug('Ship Inventory: ' + str(cargo))
+                            self.route_data.shipInventory = cargo
+                            match = match * 5
 
-                    try:
-                        scan = scan.split('"event":"CarrierStats"')
-
-                        if len(scan) != 1:
-                            del scan[-1]
-                            scan = ''.join(scan)
-                            scan = scan.split('"event":"Cargo", "Vessel":"Ship", "Count":')
-                            if len(scan) != 1:
-
-                                scan = scan[-1].split(',')[0]
-                                self.log.debug('Cargo: ' + scan)
-
-                                try:
-                                    self.route_data.shipInventory = int(scan)
-                                except Exception:
-                                    self.route_data.shipInventory = 0
-                                    self.log.exception('Ship inventory match error')
-                                    self.log.debug('Ship inventory match error cause: ' + scan)
-
-                                match = match * 5
-                    except IndexError:
-                        self.log.exception("Cargo")
-                        pass
-                if match % 7 != 0:
-
-                    scan = scanning_file
-
-                    try:
-                        scan = scan.split('"event":"CarrierStats"')
-                        if len(scan) != 1:
-
-                            scan = scan[-1].split('"FuelLevel":')[1].split(',')[0]
-                            self.log.info('Carrier fuel: ' + scan)
-
-                            try:
-                                self.route_data.carrierFuel = int(scan)
-                            except:
-                                self.route_data.carrierFuel = 0
-                                self.log.exception('Carrier fuel parse error')
-                                self.log.debug('Carrier fuel parse error cause: ' + scan)
-
+                    if match % 7 != 0:
+                        if data["event"] == "CarrierStats":
+                            fuel = data["FuelLevel"]
+                            self.route_data.carrierFuel = fuel
+                            self.log.info('Carrier fuel: ' + str(fuel))
+                            cargo = data["SpaceUsage"]["Cargo"]
+                            self.log.info('Carrier cargo: ' + str(cargo))
+                            self.route_data.carrierInventory = cargo
                             match = match * 7
-                    except IndexError:
-                        self.log.exception('Fuel')
-                        pass
-                if match % 11 != 0:
-
-                    scan = scanning_file
-
-                    try:
-                        scan = scan.split('"event":"CarrierStats"')
-                        if len(scan) != 1:
-
-                            scan = scan[-1].split('"Cargo":')[1].split(',')[0]
-                            self.log.info('Carrier cargo: ' + scan)
-
-                            try:
-                                self.route_data.carrierInventory = int(scan)
-                            except:
-                                self.route_data.carrierInventory = 0
-                                self.log.exception('Carrier cargo parse error')
-                                self.log.debug('Carrier cargo parse error cause: ' + scan)
-
-                            match = match * 11
-                    except IndexError:
-                        self.log.exception('Cargo')
-                        pass
+                    if match == target_match:
+                        break
 
         self.route_data.firstCheck = False
 
